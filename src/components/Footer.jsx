2e82bab47dc3f86bee8fef3a501f35348e7114fe
mendsfelicia <mendsfelicia@gmail.com>
2025-03-13 14:16:52 +0000

    second changesto navbar

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,14 +4,24 @@ import { Link } from "react-router";
 
 const Footer = () => {
   return (
-    <div className="bg-teal-700 text-white mt-20 p-10">
-        <div className=" flex flex-row justify-between gap-5"> 
+    <div className="bg-teal-700 text-white">
+        <div className=" flex flex-col md:flex-row justify-between gap-5 p-10 mt-20"> 
             <div>
-                <h2 className="font-bold mb-10">ReadWide Library</h2>
-                <p>Plot Number 7, legon street-osu</p>
+                <Link to="/" className="text-4xl font-extrabold tracking-wide text-white">
+                          <span className="text-yellow-300">Read</span>Wide
+                        </Link>
+               
+            </div>
+
+            <div>
+              <h2 className="font-bold mb-10">Address</h2>
+            <div className="space-y-5">
+               <p>Plot Number 7, legon street-osu</p>
                 <p>Telephone- 0503555666</p>
                 <p>email</p>
                 <a href="library@readwide.com">library@readwide.com</a>
+            </div>
+
             </div>
 
                 <div>
@@ -29,15 +39,16 @@ const Footer = () => {
 
               <div>
                 <h2 className="font-bold mb-10">Library Hours</h2>
-                <p>Mon-Friday- 8:30-22:00</p>
-                <p>Saturday-9:00-16:00</p>
-                <p>Sunday/ Holidays-Closed</p>
+                <div className="space-y-5">
+                  <p>Mon-Friday- 8:30-22:00</p>
+                  <p>Saturday-9:00-16:00</p>
+                  <p>Sunday/ Holidays-Closed</p>
+                </div>
               </div>
-              
         </div> 
-        <p className="mb-2">copyright@ Readwide Library 2025 all Rights Reserved </p>
+        <p className="mt-2 text-center">copyright@ Readwide Library 2025 all Rights Reserved </p>
     </div>
-  
+    
   )
 };
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,28 +1,30 @@
 import React from "react";
 import { Link } from "react-router";
 
+
 const Navbar = () => {
   return (
     <nav className="bg-teal-700 text-white p-4">
       <div className="container mx-auto flex justify-between items-center">
-        <h1 className="text-xl font-bold">Logo goes here</h1>
-        <ul className="flex space-x-6">
-          <li>
-            <Link to="/" className="hover:text-gray-300">Home</Link>
-          </li>
-          <li>
-            <Link to="/books" className="hover:text-gray-300">All Books</Link>
-          </li>
-          <li>
-            <Link to="/single-book" className="hover:text-gray-300">Single Book</Link>
-          </li>
-          <li>
-            <Link to="/add-book" className="hover:text-gray-300">Add Book</Link> 
-          </li>
-          <li>
-            <Link to="/contact" className="hover:text-gray-300">Contact</Link>
-          </li>  
-        </ul>
+        
+        
+        <Link to="/" className="text-3xl font-extrabold tracking-wide text-white">
+          <span className="text-yellow-300">Read</span>Wide
+        </Link>
+
+       
+        <div className="hidden md:flex md:space-x-6">
+          <Link to="/" className="hover:text-gray-300">Home</Link>
+          <Link to="/books" className="hover:text-gray-300">All Books</Link>
+          <Link to="/single-book" className="hover:text-gray-300">Single Book</Link>
+          <Link to="/add-book" className="hover:text-gray-300">Add Book</Link>
+          <Link to="/contact" className="hover:text-gray-300">Contact</Link>
+        </div>
+
+        
+        <div className="md:hidden">
+          <span className="text-2xl">☰</span>
+        </div>
       </div>
     </nav>
   );
